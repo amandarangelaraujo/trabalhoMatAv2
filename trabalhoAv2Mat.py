0d8188d50b50503864e8a41e6941942553f3f3f1
Amanda Rangel <amandarangelaraujo@gmail.com>
2023-04-27 11:49:58 -0300

    Add files via upload
    adicionei a o código para fazer uma combinação com as cartas da mesa e as cartas da mão do jogador numero 1

--- trabalhoAv2Mat.py
+++ trabalhoAv2Mat.py
@@ -1,4 +1,6 @@
 #fazer o baralho
+
+from itertools import combinations
 def gerar_baralho():
     naipes = ['copas', 'ouros', 'espadas', 'paus']
     valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'valete', 'dama', 'rei', 'ás']
@@ -31,6 +33,7 @@ def distribuir_cartas(baralho):
     return mao_jogadores, mesa
 
 #atribuindo a variáveis fora das funções o seu retorno
+
 baralho = gerar_baralho()
 mao_jogadores,mesa = distribuir_cartas(baralho)
 
@@ -40,3 +43,32 @@ jogadorNumero2=mao_jogadores[1]
 jogadorNumero3=mao_jogadores[2]
 jogadorNumero4=mao_jogadores[3]
 print(f"{jogadorNumero1} \n {jogadorNumero2} \n {jogadorNumero3} \n {jogadorNumero4}" )
+
+
+#combinar as cartas da mesa com as cartas da mão, depois fazer uma combinação dessas cartas para poder formar um trio.
+#feito apenas com um jogador, depois pensar em como diminuir esse código para fazer para os outros jogadores.
+print('##########')
+print(jogadorNumero1)
+cincoCartasJogador1=[]
+for i in range(3):
+    cincoCartasJogador1.append(jogadorNumero1[i])
+for i in range(2):
+    cincoCartasJogador1.append(mesa[i])
+
+print('##########')
+print(cincoCartasJogador1)
+
+print('calcular as possibilidades de combinações entre essas cartas:')
+comb = combinations([1, 2, 3], 2)
+trioJogador1=combinations(cincoCartasJogador1, 3)
+
+for i in list(trioJogador1):
+    print (i)
+
+
+
+#funcao de calcular os pontos
+def calcular_ponto (jogadorNumero1, jogadorNumero2, jogadorNumero3, jogadorNumero4, mesa):
+    print('oi oi oi')
+
+        
