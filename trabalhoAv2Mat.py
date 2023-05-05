320708a1bcae356536bacb9f0974cad4feafc596
Amanda Rangel <amandarangelaraujo@gmail.com>
2023-05-05 18:36:52 -0300

    Add files via upload

--- trabalhoAv2Mat.py
+++ trabalhoAv2Mat.py
@@ -1,41 +1,35 @@
+
+
 #fazer o baralho
-
 from itertools import combinations
-
 def gerar_baralho():
     naipes = ['copas', 'ouros', 'espadas', 'paus']
     valores = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
     baralho = []
-    for naipe in naipes:
-        for valor in valores:
-            baralho.append((naipe, valor))
+    for valor in valores:
+        for naipe in naipes:
+            baralho.append((valor, naipe))
     return baralho
+
 
 #distribuir as cartas, recebendo o return da função gerar_baralho
 def distribuir_cartas(baralho):
     import random
-    #mistura aleatoriamente as cartas
     random.shuffle(baralho)
     mao_jogadores = []
-    #vai distribuir 4 vezes, já que são quatro jogadores, 3 cartas
-
     for i in range (4):
-        mao=[] #a vez de que receber
-        for j in range(3): #a distribuição de cartas em si
-            mao.append(baralho.pop(0)) #remove a carta da primeira posição e salva ela na lista mão
-        mao_jogadores.append(mao) 
-      
-    mesa = []
+        mao=[]
+        for j in range(3):
+            mao.append(baralho.pop(0))
+        mao_jogadores.append(mao)
     #distribuir as cartas da mesa
+    mesa = []
     for i in range(2):
-        mesa.append(baralho.pop(0)) #tira de baralho as cartas e salva na variavel mesa
-
-    #a função retorna a variável mao, que tem as cartas dos jogadores e mesa, que tem as cartas que estão na mesa
+        mesa.append(baralho.pop(0))
     return mao_jogadores, mesa
 
-#atribuindo a variáveis fora das funções o seu retorno
-
-baralho = gerar_baralho()
+#atribuindo o resultado à variáveis fora das funções.
+baralho=gerar_baralho()
 mao_jogadores,mesa = distribuir_cartas(baralho)
 
 #definindo as cartas dos jogadores
@@ -43,13 +37,26 @@ jogadorNumero1=mao_jogadores[0]
 jogadorNumero2=mao_jogadores[1]
 jogadorNumero3=mao_jogadores[2]
 jogadorNumero4=mao_jogadores[3]
-print(f"{jogadorNumero1} \n {jogadorNumero2} \n {jogadorNumero3} \n {jogadorNumero4}" )
+print(f"O primeiro jogador recebeu as cartas: {jogadorNumero1} \n O segundo jogador recebeu: {jogadorNumero2} \n O terceiro Jogador recebeu: {jogadorNumero3} \n O quarto jogador recebeu: {jogadorNumero4}" )
 
 
-#combinar as cartas da mesa com as cartas da mão, depois fazer uma combinação dessas 
-# cartas para poder formar um trio.
+#juntar as cartas da mão de cada jogador com as cartas da mesa:
+jogadorNumero1.extend(mesa)
+jogadorNumero2.extend(mesa)
+jogadorNumero3.extend(mesa)
+jogadorNumero4.extend(mesa)
+
+print(f"juntando com as cartas da mesa o primeiro jogador tem: {jogadorNumero1} \n")
+print(f"juntando com as cartas da mesa o segundo jogador tem: {jogadorNumero2} \n")
+print(f"juntando com as cartas da mesa o terceiro jogador tem: {jogadorNumero3} \n")
+print(f"juntando com as cartas da mesa o quarto jogador tem: {jogadorNumero4} \n")
+
+#fazer todas as combinações possíveis:
+
+"""# cartas para poder formar um trio.
 #LIMPAR ESSE CÓDIGO!!!
 print('##########')
+jogadorNumero1.sort()
 print(jogadorNumero1)
 cincoCartasJogador1=[]
 for i in range(3):
@@ -106,43 +113,45 @@ print('calcular as possibilidades de combinações entre essas cartas:')
 trioJogador4=combinations(cincoCartasJogador4, 3)
 
 
-print(f'possibilidades para o jogador 1: {list(trioJogador1)}')
-print(f'possibilidades para o jogador 2: {list(trioJogador2)}')
-print(f'possibilidades para o jogador 3: {list(trioJogador3)}')
-print(f'possibilidades para o jogador 4: {list(trioJogador4)}')
 
-pontosJogador1=[]
-pontosJogador2=[]
-pontosJogador3=[]
-pontosJogador4=[]
+
+
+
+
+
+#numpy.matrix.sort
+
+
+
+#fatiamento de vetores:
+
+
 
 ##CHECAR SE DO MESMO NAIPE, SOMAR UM PONTO.
-def checarNaipeCopas():
-    print("oioioi")
-
-def checarNaipeEspadas():
-    print("oioioi")
-
-def checarNaipeOuros():
-    print("oioioi")
-
-def checarNaipePaus():
-    print("oioioi")
+#def checarNaipe(maoJogador):
+    ##lops para checar olhando a string, acessando o vetor de vetores
+    ##vários if vetor na posição tal == 'copas'
+    #   somador+1
+    #todas as combinações possíveis?
 
 
-def chegarSequência():
-    print("oioioi")
+
+
+
+
+##def checarSequência():
+    ##print("oioioi")
     #poderia fazer uma função para cada naipe que rodasse a lista e chegasse se tem uma string que armazenada esse dado
     #la em cima chamaria a função para cada jogador
     #seriam 4 funções.
     ##CHEGAR SE SEQUÊNCIA USANDO O I I-1 PARA VER SE É UMA SEQUÊNCIA
-    for i in range(60): ##ta falando que não tem len nessa type.
+    ##for i in range(60): ##ta falando que não tem len nessa type.
     #quantidade de combinaçoes possiveis==60
-        if trioJogador1[i+1]-trioJogador1[i]==1:  ##TypeError: 'itertools.combinations' object is not subscriptable
-            pontosJogador1=pontosJogador1+1
-        else:
-            continue
-print(f'o jogador numero 1 recebeu {pontosJogador1}')
+        #if trioJogador1[i+1]-trioJogador1[i]==1:  ##TypeError: 'itertools.combinations' object is not subscriptable
+            #pontosJogador1=pontosJogador1+1
+        #else:
+            #continue
+#print(f'o jogador numero 1 recebeu {pontosJogador1}')"""
 
 #função sort = sorted(trioJogador1)
 # if (trioJogador[0] + 1 == trioJogador[1] and triojogador[1] + 1 == triojogador[2])
@@ -150,3 +159,9 @@ print(f'o jogador numero 1 recebeu {pontosJogador1}')
 
 #teria que trocar os vetores pelos dicionários.
 #pecorrer os dicinários>>>>pegaria direito.
+#a função gerarBaralho fica dentro do obj jogador, jogador chama a função!!!!!! POO
+#a função recebe como parâmetro o próprio baralho.
+
+#sort para por em sequência
+#vetor de vetores, teria que acessar um elemento dentro dele e fazer o sort, como acessar esse elementos???
+#numpy.matrix.sort
